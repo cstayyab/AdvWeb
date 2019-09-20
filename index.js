d646dc0679738213c52ca1fe76098dcbc0d69867
cstayyab <cstayyab@gmail.com>
2019-09-20 16:44:23 +0500

    Create basic http Server

---
+++ index.js
@@ -0,0 +1,15 @@
+http = require('http')
+http.createServer((req, res) => {
+if(req.url=== "/" && req.method === "GET") {
+    res.writeHead(200);
+    res.write("<h1>Welcome Home</h1>")
+    res.end()
+} else if(req.url === "/account" && req.method === "GET") {
+    res.writeHead(200);
+    res.write("<h1>Welcome to Accounts</h1>")
+    res.end()
+} else {
+    res.writeHead(404);
+    res.end("<h1>404 Not Found</h1>")
+}
+}).listen(8000)
